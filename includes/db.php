51c324f79c477e238ee21ee301fe77d968059bca
Derek Garnett <drkgrntt@gmail.com>
2018-07-22 19:24:07 -0500

    connect to database

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,14 @@
 <?php
-  $connection = mysqli_connect('localhost', 'root', '', 'cms');
+  $db['host'] = 'localhost';
+  $db['user'] = 'root';
+  $db['pass'] = '';
+  $db['name'] = 'cms';
+
+  foreach($db as $key => $value) {
+    define(strtoupper($key), $value);
+  }
+
+  $connection = mysqli_connect(HOST, USER, PASS, NAME);
 
   if ($connection) {
     echo "We are connected";
